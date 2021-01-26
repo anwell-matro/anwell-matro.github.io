33efb3ba2ef0b10da43e53845b3ef98c7f4c299c
Anwell Matro <matro.aj@gmail.com>
2021-01-26 02:04:44 -0500

    Added CNAME

---
+++ deploy.ps1
@@ -0,0 +1,7 @@
+﻿git pull
+git add .
+git commit -m "Added CNAME"
+git push root gh-pages:master -f
+
+Write-Host -NoNewLine 'Press any key to continue...';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
